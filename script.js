1b0ce4e846f8cdd95294f3c387b11428c64aae5d
aqtagon <ahmed.danawi@gmail.com>
2023-10-01 18:53:02 -0700

    Added dayjs for time-blocks

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ $(function () {
 
   var currentHour = dayjs().hour();
 
-  
+  $(".time-block").each(function (){
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
 });
 
 
