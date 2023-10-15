a7f76347bf4d169db32a68854186a5d3a931b12c
aqtagon <ahmed.danawi@gmail.com>
2023-10-14 22:07:43 -0700

    Fixed color coding issue

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
   var currentDay = dayjs().format("dddd, MMMM D")
   $("#currentDay").text(currentDay);
 
   var currentHour = dayjs().hour();
 
-  $(".time-block").each(function (){
+  $(".time-block").each(function () {
     var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
     if (blockHour < currentHour) {
-      $(this).addClass("past");
+      $(this).removeClass("present future").addClass("past");
     } else if (blockHour === currentHour) {
-      $(this).addClass("present");
+      $(this).removeClass("past future").addClass("present");
     } else {
-      $(this).addClass("future");
+      $(this).removeClass("past present").addClass("future");
     }
   });
   
